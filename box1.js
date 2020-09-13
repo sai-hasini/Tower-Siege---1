00e249a59cf2306bcccbd882919a98f246e78d0f
sai-hasini <68668741+sai-hasini@users.noreply.github.com>
2020-09-12 20:57:16 -0700

    Add files via upload

---
+++ box1.js
@@ -0,0 +1,29 @@
+class box1 {
+    constructor(x, y, width, height) {
+      var options = {
+           isStatic : false,
+           restitution:0.4,
+           friction:0.5
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(3)
+      stroke("black")
+      fill(255,104,153);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
